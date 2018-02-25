50911469a23755d6d68d1955afe5ba1c2b00740a
Nick Mote <njmote@gmail.com>
2018-02-25 10:37:29 -0800

    adding -j flag to keep in root dir

--- src/pip-and-zip.sh
+++ src/pip-and-zip.sh
@@ -4,7 +4,7 @@
 pip install -t . ${pip}
 
 #package contents in zip for lambda
-zip -r linux-lambda.zip /working
+zip -j -r linux-lambda.zip /working
 
 ####DO NOT REMOVE OR EDIT BELOW THIS LINE ####
 ####IF YOU DO, USE DOS2UNIX TO FIX ###########
